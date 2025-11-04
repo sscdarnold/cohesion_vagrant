f6100d6d224de71ee16b4df86a9c290776adc82a
Derek Arnold <darnold@starkstate.edu>
2025-11-04 15:36:41 -0500

    removed files in .vagrant

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["racc",["~> 1.4"]],["nokogiri",[">= 0"]],["vagrant-parallels",["= 2.4.6"]],["vagrant-vmware-desktop",["= 3.0.5"]]],"checksum":"c0b1a1674de6c4a97f089a5ad5f6ffc961fd3186f87f573b787355ee3e8c31d9","vagrant_version":"2.4.9"}
